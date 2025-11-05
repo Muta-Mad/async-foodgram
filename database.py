8d42761f5936763ec9b4a6da22cb2c617894f500
Tagaev <tagaevmukhammad@gmail.com>
2025-11-05 19:24:37 +0300

    first commit

---
+++ database.py
@@ -0,0 +1,45 @@
+import sys
+import asyncio
+
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+from src.api.models import Base
+
+
+engine = create_async_engine('sqlite+aiosqlite:///./sqlite.db')
+
+new_session = async_sessionmaker(bind=engine, expire_on_commit=False)
+
+
+async def get_db():
+    async with new_session() as session:
+        try:
+            yield session
+        finally:
+            await session.close()
+
+async def create_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def drop_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
+async def create_db():
+    await create_tables()
+    print('database created!')
+
+async def drop_db():
+    await drop_tables()
+    print('database drop!')
+
+def main():
+    command = sys.argv[1]
+    if command == 'create_db':
+        asyncio.run(create_db())
+    if command == 'drop_db':
+        asyncio.run(drop_db()) 
+if __name__ == "__main__":
+    main()
---
+++ src/api/models.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String
+
+from src.api.constants import (MAX_LENGTH_FIRST_NAME, 
+                               MAX_LENGTH_LAST_NAME, 
+                               MAX_LENGTH_EMAIL
+                               )
+
+
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = 'users'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    first_name: Mapped[str] = mapped_column(String(MAX_LENGTH_FIRST_NAME), unique=True)
+    last_name: Mapped[str] = mapped_column(String(MAX_LENGTH_LAST_NAME), unique=True)
+    email: Mapped[str] = mapped_column(String(MAX_LENGTH_EMAIL), unique=True)
