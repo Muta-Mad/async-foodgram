3b307a7791f89ca846481964a3e258e66993cee1
Tagaev <tagaevmukhammad@gmail.com>
2025-11-29 15:46:04 +0300

    add_to requirements.txt dependens

--- alembic/versions/2025_11_29_1531-f3e59afed8dc_fix_db.py
+++ alembic/versions/2025_11_29_1531-f3e59afed8dc_fix_db.py
@@ -1,8 +1,8 @@
-"""рефактор
+"""fix_db
 
-Revision ID: cdbe344907c3
+Revision ID: f3e59afed8dc
 Revises: 
-Create Date: 2025-11-29 15:14:29.266388
+Create Date: 2025-11-29 15:31:02.045342
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'cdbe344907c3'
+revision: str = 'f3e59afed8dc'
 down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
