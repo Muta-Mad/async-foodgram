3328dd383342566e0ce4568d9e25f765b979c812
Tagaev <tagaevmukhammad@gmail.com>
2025-12-01 16:45:10 +0300

    add_Ingredients_table

--- api/__init__.py
+++ api/__init__.py
@@ -1,7 +1,12 @@
 __all__ = (
-    'User', 'Tag', 'Base', 'AccessToken'
+    'User', 
+    'Tag', 
+    'Base', 
+    'AccessToken',
+    'Ingredient',
 )
 
 from api.tags.models import Tag
 from api.users.models import AccessToken, User
+from api.ingredients.models import Ingredient
 from api.basemodel import Base
