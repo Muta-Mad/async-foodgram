184c0598b5dcf66758a0e7f26c7880c6f1c92bbf
Tagaev <tagaevmukhammad@gmail.com>
2025-12-08 15:38:14 +0300

    fix_routers_2

--- api/users/auth/backend.py
+++ api/users/auth/backend.py
@@ -4,7 +4,7 @@ from api.users.auth.transport import bearer_transport
 from api.users.auth.strategies import get_database_strategy
 
 authentication_backend = AuthenticationBackend(
-    name='access-token',
+    name='auth_token',
     transport=bearer_transport,
     get_strategy=get_database_strategy,
 )
